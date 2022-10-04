2294f55d47db65d343696e0c41451a6f8ca03d05
AnreBr <andrejs.e.broks@gmail.com>
2022-10-04 13:40:47 +0300

    Class_07_08

---
+++ Class_07/scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+  char str [80];
+  int i;
+
+  printf ("vārds: ");
+  scanf ("%79s",str);  
+  printf ("vecums: ");
+  scanf ("%d",&i);
+}
---
+++ test1.sh
@@ -0,0 +1,6 @@
+echo"ievadi PAT token"
+
+read PAT
+
+
+git push https://<$PAT>@github.com/<AnreBr>/<RTR105>.git
