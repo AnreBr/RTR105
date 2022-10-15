71b06a7b6e324887320bacd7ec221e2fa544df10
Andrejs <andrejs@localhost.localdomain>
2022-10-16 01:20:08 +0300

    M_16_10

---
+++ program/40000000.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+   {
+   long long int a, b, c;
+
+   printf ("ievadi 400000\n");
+   scanf ("%lli", &a);
+
+   printf ("ievadi 100000\n");
+   scanf ("%lli", &b);
+   
+   c=a*b;
+   printf ("%lli,\n", c ); 
+   return 0; 
+   }
