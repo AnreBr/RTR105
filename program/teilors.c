86f3195979c551067176c5b93efe9b55d01baf34
AnreBr <112925682+AnreBr@users.noreply.github.com>
2022-11-29 09:49:42 +0200

    Add files via upload

---
+++ program/teilors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+double x, y, S, a;
+int k = 0;
+
+int main()
+{
+
+   printf("ievadi savu x vērtību\n");
+   scanf("%lf\n",&x1);
+
+   a = pow(x,k)/factorial(2*k);
+   S = a;
+   printf ("x = %f, a = %f, S = %f",x,a,S);
+
+   
+
+return 0;
+}
